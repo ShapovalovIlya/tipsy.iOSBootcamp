8e3b60b5902e5d0425b497b8d7ff731c20371751
Ilia Shapovalov <greateman@mail.ru>
2022-04-19 00:22:01 +0300

    Work in progress. Sometimes i think i'll lose my mind due to this optionals...

--- Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -10,19 +10,27 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
-    @IBOutlet weak var zeroTipsLabel: UIButton!    
+    @IBOutlet weak var totalBillField: UITextField!
+    @IBOutlet weak var zeroTipsLabel: UIButton!
     @IBOutlet weak var tenTipsLabel: UIButton!
     @IBOutlet weak var twentyTipsLabel: UIButton!
     @IBOutlet weak var stepperLabel: UIStepper!
     @IBOutlet weak var splitLabel: UILabel!
     
     var tip = 0.0
-    var split = 0
+    var split = 2.0
+    var totalBill = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        totalBillField.keyboardType = .numbersAndPunctuation
+        tenTipsLabel.isSelected = true
+        splitLabel.text = "2"
+    }
+    //Overrade method to hide keyboard then tapping outside text field
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 
     @IBAction func tipChanged(_ sender: UIButton) {
@@ -43,14 +51,23 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func splitTapped(_ sender: UIStepper) {
+        //Sett minimum and maximum values for stepper
         sender.minimumValue = 1
         sender.maximumValue = 12
-        let totalSplit = Int(sender.value)
-        splitLabel.text = String(totalSplit)
+        
+        split = sender.value
+        //Convert split value into string and set label
+        splitLabel.text = String(format: "%.0f", split)
     }
     
     @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        let unwrappedBillField = totalBillField.text ?? "0.0"
+        totalBill = Double(unwrappedBillField)!
+        let totalTip = (totalBill + (totalBill * tip)) / split
+        print(totalBill)
         print(tip)
+        print(split)
+        print(totalTip)
     }
     
 }
